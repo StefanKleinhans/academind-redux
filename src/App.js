edbaf744761134a9de48eef3e963accb1039e6af
Stefan Kleinhans <stefan.kleinhans2802@gmail.com>
2022-08-16 09:16:47 +0200

    Called Redux Functions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { useSelector, useDispatch } from "react-redux";
 import { Fragment } from "react";
 import Counter from "./components/Counter";
 import Header from "./components/Header";
 import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+import { authActions } from './store/index'
 
 function App() {
+
+  const dispatch = useDispatch();
+  const showUser = useSelector((state) => state.auth.isAuthenticated);
+
   return (
     <Fragment>
       <Header />
       <Auth />
+      {showUser && <UserProfile />}
       <Counter />
     </Fragment>
   );
